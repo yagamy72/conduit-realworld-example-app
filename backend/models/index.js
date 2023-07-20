d65f531e3d2fa02f26259c2a70565e27a60ff6a8
yagamy72 <138251857+yagamy72@users.noreply.github.com>
2023-07-20 13:17:06 +0000

    chore: send config log

--- backend/models/index.js
+++ backend/models/index.js
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
+console.log('config', config);
 const sequelize = new Sequelize(
   config.database,
   config.username,
